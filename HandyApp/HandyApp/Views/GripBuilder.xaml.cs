9b4feb87626b1650a25ab2199d5cc0f699ad97c0
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-01-16 17:06:15 +0000

    Starting add the other pages to the App that are needed...

---
+++ HandyApp/HandyApp/Views/GripBuilder.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HandyApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GripBuilder : ContentPage
+    {
+        public GripBuilder()
+        {
+            InitializeComponent();
+        }
+    }
+}
