aefca90e0b14bb51d62f1825a10e444093519ab1
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-01-16 13:27:21 +0000

    Have it now working where it will connect and data can be sent to/from the device, Handy responses are missing some data for some reason but we can look at that.  Good news is that this version can control Handy which is amazing...

--- HandyApp/HandyApp/ViewModels/DeviceConnectionViewModel.cs
+++ HandyApp/HandyApp/ViewModels/DeviceConnectionViewModel.cs
@@ -25,7 +25,7 @@ namespace HandyApp.ViewModels
         private Guid DeviceId { get; set; }
 
 
-        //public ObservableList<String> BTDataRcvd { get; private set; } = new ObservableList<String>();
+        public ObservableRangeCollection<string> BTDataRcvd { get; private set; } = new ObservableRangeCollection<string>();
 
         Guid serviceGuid = Guid.Parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
         Guid writeGuid = Guid.Parse("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
@@ -68,10 +68,16 @@ namespace HandyApp.ViewModels
 
         private void ReadCharacteristic_ValueUpdated(object sender, Plugin.BLE.Abstractions.EventArgs.CharacteristicUpdatedEventArgs e)
         {
-            if (readCharacteristic.CanRead)
+            MainThread.BeginInvokeOnMainThread(() =>
             {
-                var test = readCharacteristic.ReadAsync();
-            }
+                if (e.Characteristic?.Value is object)
+                {
+
+                    var rcvdValue = Encoding.UTF8.GetString(e.Characteristic.Value);
+                    //var rcvdValue = await readCharacteristic.ReadAsync().ConfigureAwait(false);
+                    BTDataRcvd.Add(rcvdValue);
+                }
+            });
         }
 
         private async Task Connect()
@@ -89,7 +95,7 @@ namespace HandyApp.ViewModels
                 readCharacteristic = await service.GetCharacteristicAsync(readGuid).ConfigureAwait(false);
 
                 readCharacteristic.ValueUpdated += ReadCharacteristic_ValueUpdated;
-                await readCharacteristic.StartUpdatesAsync();
+                await readCharacteristic.StartUpdatesAsync().ConfigureAwait(false);
             }
             catch (Exception ex)
             {
@@ -98,89 +104,3 @@ namespace HandyApp.ViewModels
         }
     }
 }
-
-//    Hand.Connect();
-
-//    // this will watch the connection states to the device
-//    Hand.WhenStatusChanged().Subscribe(async connectionState =>
-//    {
-//        Dialogs.Toast($"Connection state has changed - {connectionState.ToString()}");
-//        try
-//        {
-//            Hand = await CrossBleAdapter.Current.GetKnownDevice(DeviceId).FirstOrDefaultAsync();
-//        }
-//        catch
-//        {
-//            Dialogs.Toast("Unable to connect...");
-//        }
-//    }, ex =>
-//    {
-//        Dialogs.Toast($"Status Change Error - {ex.Message}", TimeSpan.FromSeconds(3));
-//    });
-
-//    //Command that sends the UART data to the Hand...
-//    UARTCommand = ReactiveCommand.Create<string>(UARTString =>
-//    {
-//        UARTString += "\n\r";
-
-//        byte[] bytes = Encoding.ASCII.GetBytes(UARTString);
-
-//        Hand.GetKnownCharacteristics(serviceGuid, writeGuid)
-//        .ObserveOn(RxApp.MainThreadScheduler)
-//        .Subscribe(characteristic =>
-//        {
-//            if (characteristic.CanWrite())
-//            {
-//                characteristic.WriteWithoutResponse(bytes).Subscribe(result =>
-//                {
-//                    // you don't really need to do anything with the result
-//                    string str = Encoding.Default.GetString(result.Data);
-//                    Dialogs.Toast($"Message Sent - {str}");
-//                },
-//                exception =>
-//                {
-//                    Dialogs.Toast($"Write Error - {exception.Message}");
-//                });
-//            }
-//        },
-//        ex =>
-//        {
-//            Dialogs.Toast($"Write Error - {ex.Message}", TimeSpan.FromSeconds(3));
-//        });
-//    });
-
-//    //Recieve Data from Hand...
-//    Hand.ConnectHook(serviceGuid, readGuid).Subscribe(result =>
-//    {
-//        string str = Encoding.Default.GetString(result.Data);
-//        Dialogs.Toast($"Message Recv'd - {str}", TimeSpan.FromSeconds(3));
-
-//        BTDataRcvd.Insert(0, str);
-//    }, ex =>
-//    {
-//        Dialogs.Toast($"Read Error - {ex.Message}", TimeSpan.FromSeconds(3));
-//    });
-
-//    PairToDevice = ReactiveCommand.Create(() =>
-//    {
-//        if (!Hand.Features.HasFlag(DeviceFeatures.PairingRequests))
-//        {
-//            Dialogs.Toast("Pairing is not supported on this platform");
-//        }
-//        else if (Hand.PairingStatus == PairingStatus.Paired)
-//        {
-//            Dialogs.Toast("Device is already paired");
-//        }
-//        else
-//        {
-//            Hand.PairingRequest()
-//                .Subscribe(x =>
-//                {
-//                    var txt = x ? "Device Paired Successfully" : "Device Pairing Failed";
-//                    Dialogs.Toast(txt);
-//                }, ex =>
-//                {
-//                    Dialogs.Toast($"Pairing Error - {ex.Message}", TimeSpan.FromSeconds(3));
-//                });
-//        }
-//    });
