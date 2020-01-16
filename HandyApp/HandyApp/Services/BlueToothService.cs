d8f660295c0a95a247bf89e0d92f9e9fe8bb6064
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-01-16 14:55:30 +0000

    Starting to build out a BT Service so that it can be shared around the App...

---
+++ HandyApp/HandyApp/Services/BlueToothService.cs
@@ -0,0 +1,90 @@
+﻿using MvvmHelpers;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace HandyApp.Services
+{
+    public class BlueToothService
+    {
+        Guid serviceGuid = Guid.Parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+        Guid writeGuid = Guid.Parse("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+        Guid readGuid = Guid.Parse("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+       
+        private ICharacteristic writeCharacteristic;
+        private ICharacteristic readCharacteristic;
+        
+        public string RcvdDataString { get; set; }
+        public ObservableCollection<string> BTDataRcvd { get; set; }
+
+        public BlueToothService()
+        {
+
+        }
+
+        public IAdapter Adapter { get; private set; }
+        public IDevice Hand { get; private set; }
+
+        public async Task Connect()
+        {
+            try
+            {
+                Adapter = CrossBluetoothLE.Current.Adapter;
+                await Adapter.ConnectToDeviceAsync(Hand);
+                var service = await Hand.GetServiceAsync(serviceGuid).ConfigureAwait(false);
+
+                var characteristics = await service.GetCharacteristicsAsync();
+
+
+                writeCharacteristic = await service.GetCharacteristicAsync(writeGuid).ConfigureAwait(false);
+                readCharacteristic = await service.GetCharacteristicAsync(readGuid).ConfigureAwait(false);
+
+                readCharacteristic.ValueUpdated += ReadCharacteristic_ValueUpdated;
+                await readCharacteristic.StartUpdatesAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Sorry an error while sending command - {ex.Message}");
+            }
+        }
+
+        public async Task SendUARTCommand(string UARTString)
+        {
+            try
+            {
+                UARTString += "\n\r";
+
+                byte[] bytes = Encoding.ASCII.GetBytes(UARTString);
+                await writeCharacteristic.WriteAsync(bytes).ConfigureAwait(false);
+                //var test = readCharacteristic.ReadAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Sorry an error while sending command - {ex.Message}");
+            }
+        }
+
+        private void ReadCharacteristic_ValueUpdated(object sender, Plugin.BLE.Abstractions.EventArgs.CharacteristicUpdatedEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                if (e.Characteristic?.Value is object)
+                {
+                    var rcvdValue = Encoding.UTF8.GetString(e.Characteristic.Value);
+                    RcvdDataString += rcvdValue;
+                    if (e.Characteristic.Value[e.Characteristic.Value.Length - 1] == 10)
+                    {
+                        BTDataRcvd.Add(RcvdDataString);
+                        RcvdDataString = "";
+                    }
+                }
+            });
+        }
+    }
+}
