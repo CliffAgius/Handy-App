d442b1b5d89b78659ff480266095ff5b387a6411
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-01-18 19:18:49 +0000

    Fixed some file naming issues and added the VM for the HandControl in prep for getting the buttons to do something...

---
+++ HandyApp/HandyApp/ViewModels/HandControlViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandyApp.ViewModels
+{
+    class HandControlViewModel
+    {
+    }
+}
---
+++ HandyApp/HandyApp/Views/GripBuilderView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HandyApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GripBuilderView : ContentPage
+    {
+        public GripBuilderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
