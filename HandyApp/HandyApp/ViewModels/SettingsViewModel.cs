4d2212909da3039975a3ae2155f7d77e1c7b5bac
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-01-22 21:06:05 +0000

    Added the Settings VM and wired it to the View ready to populate...

---
+++ HandyApp/HandyApp/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Acr.UserDialogs;
+
+namespace HandyApp.ViewModels
+{
+    public class SettingsViewModel
+    {
+        private IUserDialogs Dialogs;
+
+        public SettingsViewModel(IUserDialogs dialog)
+        {
+            Dialogs = dialog;
+        }
+    }
+}
--- HandyApp/HandyApp/Views/SettingsView.xaml.cs
+++ HandyApp/HandyApp/Views/SettingsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using Acr.UserDialogs;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace HandyApp.Views
@@ -9,6 +10,8 @@ namespace HandyApp.Views
         public SettingsView()
         {
             InitializeComponent();
+
+            BindingContext = new ViewModels.SettingsViewModel(UserDialogs.Instance);
         }
     }
 }
