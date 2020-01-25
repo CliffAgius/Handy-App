e881bfd681d4d9fde8747d0b039ada29d276154a
Clifford Agius <CliffordAgius@CA-Software.co.uk>
2020-01-25 18:22:24 +0000

    Added the new shiney swipeview to the GripListPage so that you can swipe to delete from the list... Very good but yet to implement the Save Button action as that needs code changes to Handy as well...

--- HandyApp/HandyApp/ViewModels/GripListViewModel.cs
+++ HandyApp/HandyApp/ViewModels/GripListViewModel.cs
@@ -3,19 +3,35 @@ using HandyApp.Models;
 using MvvmHelpers;
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace HandyApp.ViewModels
 {
     public class GripListViewModel : BaseViewModel
     {
-
+        public Command<Grip> DeleteGripCommand { get; private set; }
         public ObservableCollection<Grip> Grips { get; private set; } = new ObservableCollection<Grip>();
 
         public GripListViewModel()
         {
             GripsCollection gripsCollection = new GripsCollection();
             Grips = gripsCollection.CreateGripsCollection();
+
+            DeleteGripCommand = new Command<Grip>(ActionDeleteGripCommand);
+        }
+
+        private void ActionDeleteGripCommand(Grip obj)
+        {
+            try
+            {
+                Grips.Remove(obj);
+                OnPropertyChanged();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error deleting the grip - {ex.Message}");
+            }
         }
     }
 }
